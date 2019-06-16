e4530cb518147952cc1a21facbcea57a11a624fd
N3ov <46546577+N3ov@users.noreply.github.com>
2019-06-17 02:12:24 +0800

    Add files via upload

---
+++ hw1-1.js
@@ -0,0 +1,24 @@
+const byId = (id) => document.getElementById(id)
+
+function pool(size, lowest, highest) {
+	var numbers = [];
+	for (let i = 0; i < size; i++) {
+		let add = true;
+		let randomNumber = Math.floor(Math.random() * highest) + 1;
+		for (let j = 0; j < highest; j++) {
+			if (numbers[j] == randomNumber) {
+				add = false;
+			}
+		}
+		if (add) {
+			numbers.push(randomNumber);
+		} else {
+			i--;
+		}
+	}
+	byId("numbers").innerHTML = numbers.join('-');
+	byId("len").innerHTML = numbers.length
+	byId("1st").innerHTML = numbers[0]
+
+
+}
